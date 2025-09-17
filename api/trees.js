7ecd02ae9b1706df60e3988c86d2c43288c0480e
eamonnhanson <eamonn_hanson@yahoo.co.uk>
2025-09-17 14:36:00 +0200

    Update trees.js

--- api/trees.js
+++ api/trees.js
@@ -1,23 +1,12 @@
-// api/trees.js
 import pkg from "pg";
 const { Client } = pkg;
 
 export default async function treesHandler(req, context) {
   try {
+    const client = new Client({
+      connectionString: process.env.PG_URL,
+    });
 
-
-
-const client = new Client({
-  connectionString: process.env.PG_URL,
-  ssl: {
-    ca: process.env.PG_CA_CERT.replace(/\\n/g, "\n"), // 🔑 fix for Render env vars
-    rejectUnauthorized: true,
-  },
-});
-
-
-
-    
     await client.connect();
     const result = await client.query("SELECT 1 as test");
     await client.end();
