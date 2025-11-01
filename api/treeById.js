fea0d77c6470b697b70db5294a1209ca64ee1701
eamonnhanson <eamonn_hanson@yahoo.co.uk>
2025-11-01 15:19:07 +0100

    Create treeById.js

---
+++ api/treeById.js
@@ -0,0 +1,22 @@
+// GET /api/trees/:id
+export default async function treeByIdHandler(req, res) {
+  try {
+    const { id } = req.params;
+    if (!id) return res.status(400).json({ ok: false, error: "id ontbreekt" });
+
+    // TODO: vervang door echte database query
+    const item = {
+      id,
+      tree_code: null,
+      tree_name: null,
+      tree_type: null,
+      lat: null,
+      long: null
+    };
+
+    res.json({ ok: true, item });
+  } catch (err) {
+    console.error("treeById error:", err);
+    res.status(500).json({ ok: false, error: "internal error" });
+  }
+}
